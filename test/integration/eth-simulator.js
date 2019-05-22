9d6b695c54d61de8f33b334d911d53298d4bdb3c
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-21 23:17:32 -0300

    Update eth-simulator.js

--- test/integration/eth-simulator.js
+++ test/integration/eth-simulator.js
@@ -8,8 +8,8 @@ const GENESIS_TD = 17179869184
 const GENESIS_HASH = Buffer.from('b4973da140b05bfffb1cd734ed871f888e71cf563a4218f82a092fc4540f6c03', 'hex')
 
 var capabilities = [
-  devp2p.ETH.eth63,
-  devp2p.ETH.eth62
+  devp2p.PWP.eth63,
+  devp2p.PWP.eth62
 ]
 
 const status = {
@@ -22,7 +22,7 @@ const status = {
 // FIXME: Handle unhandled promises directly
 process.on('unhandledRejection', (reason, p) => {})
 
-test('ETH: send status message (successful)', async (t) => {
+test('PWP: send status message (successful)', async (t) => {
   let opts = {}
   opts.status0 = Object.assign({}, status)
   opts.status1 = Object.assign({}, status)
@@ -34,7 +34,7 @@ test('ETH: send status message (successful)', async (t) => {
   util.twoPeerMsgExchange(t, capabilities, opts)
 })
 
-test('ETH: send status message (NetworkId mismatch)', async (t) => {
+test('PWP: send status message (NetworkId mismatch)', async (t) => {
   let opts = {}
   opts.status0 = Object.assign({}, status)
   let status1 = Object.assign({}, status)
@@ -49,7 +49,7 @@ test('ETH: send status message (NetworkId mismatch)', async (t) => {
   util.twoPeerMsgExchange(t, capabilities, opts)
 })
 
-test('ETH: send status message (Genesis block mismatch)', async (t) => {
+test('PWP: send status message (Genesis block mismatch)', async (t) => {
   let opts = {}
   opts.status0 = Object.assign({}, status)
   let status1 = Object.assign({}, status)
@@ -64,17 +64,17 @@ test('ETH: send status message (Genesis block mismatch)', async (t) => {
   util.twoPeerMsgExchange(t, capabilities, opts)
 })
 
-test('ETH: send allowed eth63', async (t) => {
+test('PWP: send allowed eth63', async (t) => {
   let opts = {}
   opts.status0 = Object.assign({}, status)
   opts.status1 = Object.assign({}, status)
   opts.onOnceStatus0 = function (rlpxs, eth) {
     t.equal(eth.getVersion(), 63, 'should use eth63 as protocol version')
-    eth.sendMessage(devp2p.ETH.MESSAGE_CODES.NEW_BLOCK_HASHES, [ 437000, 1, 0, 0 ])
+    eth.sendMessage(devp2p.PWP.MESSAGE_CODES.NEW_BLOCK_HASHES, [ 437000, 1, 0, 0 ])
     t.pass('should send NEW_BLOCK_HASHES message')
   }
   opts.onOnMsg1 = function (rlpxs, eth, code, payload) {
-    if (code === devp2p.ETH.MESSAGE_CODES.NEW_BLOCK_HASHES) {
+    if (code === devp2p.PWP.MESSAGE_CODES.NEW_BLOCK_HASHES) {
       t.pass('should receive NEW_BLOCK_HASHES message')
       util.destroyRLPXs(rlpxs)
       t.end()
@@ -83,19 +83,19 @@ test('ETH: send allowed eth63', async (t) => {
   util.twoPeerMsgExchange(t, capabilities, opts)
 })
 
-test('ETH: send allowed eth62', async (t) => {
+test('PWP: send allowed eth62', async (t) => {
   let cap = [
-    devp2p.ETH.eth62
+    devp2p.PWP.eth62
   ]
   let opts = {}
   opts.status0 = Object.assign({}, status)
   opts.status1 = Object.assign({}, status)
   opts.onOnceStatus0 = function (rlpxs, eth) {
-    eth.sendMessage(devp2p.ETH.MESSAGE_CODES.NEW_BLOCK_HASHES, [ 437000, 1, 0, 0 ])
+    eth.sendMessage(devp2p.PWP.MESSAGE_CODES.NEW_BLOCK_HASHES, [ 437000, 1, 0, 0 ])
     t.pass('should send NEW_BLOCK_HASHES message')
   }
   opts.onOnMsg1 = function (rlpxs, eth, code, payload) {
-    if (code === devp2p.ETH.MESSAGE_CODES.NEW_BLOCK_HASHES) {
+    if (code === devp2p.PWP.MESSAGE_CODES.NEW_BLOCK_HASHES) {
       t.pass('should receive NEW_BLOCK_HASHES message')
       util.destroyRLPXs(rlpxs)
       t.end()
@@ -104,16 +104,16 @@ test('ETH: send allowed eth62', async (t) => {
   util.twoPeerMsgExchange(t, cap, opts)
 })
 
-test('ETH: send not-allowed eth62', async (t) => {
+test('PWP: send not-allowed eth62', async (t) => {
   let cap = [
-    devp2p.ETH.eth62
+    devp2p.PWP.eth62
   ]
   let opts = {}
   opts.status0 = Object.assign({}, status)
   opts.status1 = Object.assign({}, status)
   opts.onOnceStatus0 = function (rlpxs, eth) {
     try {
-      eth.sendMessage(devp2p.ETH.MESSAGE_CODES.GET_NODE_DATA, [])
+      eth.sendMessage(devp2p.PWP.MESSAGE_CODES.GET_NODE_DATA, [])
     } catch (err) {
       const msg = 'Error: Code 13 not allowed with version 62'
       t.equal(err.toString(), msg, `should emit error: ${msg}`)
@@ -124,7 +124,7 @@ test('ETH: send not-allowed eth62', async (t) => {
   util.twoPeerMsgExchange(t, cap, opts)
 })
 
-test('ETH: send unknown message code', async (t) => {
+test('PWP: send unknown message code', async (t) => {
   let opts = {}
   opts.status0 = Object.assign({}, status)
   opts.status1 = Object.assign({}, status)
@@ -141,13 +141,13 @@ test('ETH: send unknown message code', async (t) => {
   util.twoPeerMsgExchange(t, capabilities, opts)
 })
 
-test('ETH: invalid status send', async (t) => {
+test('PWP: invalid status send', async (t) => {
   let opts = {}
   opts.status0 = Object.assign({}, status)
   opts.status1 = Object.assign({}, status)
   opts.onOnceStatus0 = function (rlpxs, eth) {
     try {
-      eth.sendMessage(devp2p.ETH.MESSAGE_CODES.STATUS, [])
+      eth.sendMessage(devp2p.PWP.MESSAGE_CODES.STATUS, [])
     } catch (err) {
       const msg = 'Error: Please send status message through .sendStatus'
       t.equal(err.toString(), msg, `should emit error: ${msg}`)
